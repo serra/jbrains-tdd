e32eaad842c531a95c052804105d6468e060e918
Marijn van der Zee <marijn@serraict.com>
2018-06-11 21:43:03 +0200

    Add package and publish scripts

--- python/setup.py
+++ python/setup.py
@@ -13,8 +13,8 @@ setup(
     # metadata for upload to PyPI
     author="Marijn van der Zee",
     author_email="marijn@serraict.com",
-    description="Generate a static site from a Github wiki",
+    description="My code for JBrains' TDD course",
     license="GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007",
-    keywords="",
+    keywords="tdd jbrains",
     url="https://www.serraict.com",
 )
