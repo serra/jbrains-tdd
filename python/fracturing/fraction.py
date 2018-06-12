605853f3e2c37fb85c5750392f2d00d1f21f32b7
Marijn van der Zee <marijn@serraict.com>
2018-06-12 09:45:51 +0200

    Use __repr__ instead of __str__
    The default __str__ implementation calls __repr__.
    In the case of Fraction, this __repr__ unambiguously represents our fraction.

--- python/fracturing/fraction.py
+++ python/fracturing/fraction.py
@@ -25,5 +25,5 @@ class Fraction:
         sum_numerator = other._den * self._num + other._num * self._den
         return Fraction(sum_numerator, common_denominator)
 
-    def __str__(self):
+    def __repr__(self):
         return f'{self._num}/{self._den}'
