a17e0f6ce57ac5faffcdaa1b1e8a62cdb7688471
Marijn van der Zee <marijn@serraict.com>
2018-06-12 08:28:47 +0200

    Add smoke test

--- python/tests/packaging.py
+++
@@ -1,4 +0,0 @@
-print('hello world')
-
-# from serrajbrainstdd.fraction import arithmetic
- # arithmetic._gcd(6, 3)
---
+++ python/tests/smoketest.py
@@ -0,0 +1,4 @@
+from fraction.arithmetic import Fraction
+print('=====')
+print(str(Fraction(5, 3) + Fraction(2, 7)))
+print('=====')
