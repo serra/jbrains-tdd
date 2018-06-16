2de3638dc267a177e1243cf5e048b4bb0b092de4
Marijn van der Zee <marijn@serraict.com>
2018-06-16 14:51:27 +0200

    Subtract with negative result

--- python/tests/subtract_fractions_test.py
+++ python/tests/subtract_fractions_test.py
@@ -7,5 +7,8 @@ class SubtractFractionsTestCase(unittest.TestCase):
     def test_can_subtract_fractions(self):
         assert Fraction(2, 3) - Fraction(1, 3) is not None
 
-    def test_subtract_whole_numbers_gt_zero(self):
+    def test_subtract_whole_numbers_positive_result(self):
         assert Fraction(5) - Fraction(3) == Fraction(2)
+
+    def test_subtract_whole_numbers_negative_result(self):
+        assert Fraction(3) - Fraction(5) == Fraction(-2)
