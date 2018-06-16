09754767c1855621e35312e0f9dd911808f1b90a
Marijn van der Zee <marijn@serraict.com>
2018-06-16 13:47:05 +0200

    Get rid of the unnecessary indirection _f

--- python/tests/add_fractions_test.py
+++ python/tests/add_fractions_test.py
@@ -1,19 +1,19 @@
 from .context import fracturing
 import unittest
-from .fraction_tests import _f
+from fracturing.fraction import Fraction
 
 
 class AddFractionsTestCase(unittest.TestCase):
     def test_add_fractions(self):
-        assert _f(1, 3) + _f(1, 2) == _f(5, 6)
-        assert _f(1, 6) + _f(1, 3) == _f(1, 2)
-        assert _f(1, 2) + _f(1, 2) == _f(1, 1)
-        assert _f(1, 12) + _f(2, 12) == _f(1, 4)
-        assert _f(7, 9) + _f(5, 9) == _f(4, 3)
+        assert Fraction(1, 3) + Fraction(1, 2) == Fraction(5, 6)
+        assert Fraction(1, 6) + Fraction(1, 3) == Fraction(1, 2)
+        assert Fraction(1, 2) + Fraction(1, 2) == Fraction(1, 1)
+        assert Fraction(1, 12) + Fraction(2, 12) == Fraction(1, 4)
+        assert Fraction(7, 9) + Fraction(5, 9) == Fraction(4, 3)
 
     def test_add_with_zeros(self):
-        assert _f(0, 2) + _f(0, 2) == _f(0, 2)
-        assert _f(0, 13) + _f(1, 2) == _f(1, 2)
+        assert Fraction(0, 2) + Fraction(0, 2) == Fraction(0, 2)
+        assert Fraction(0, 13) + Fraction(1, 2) == Fraction(1, 2)
 
     def test_add_like_normal_numbers_use_plus_operator(self):
-        assert _f(1, 3) + _f(1, 2) == _f(5, 6)
+        assert Fraction(1, 3) + Fraction(1, 2) == Fraction(5, 6)
--- python/tests/fraction_tests.py
+++ python/tests/fraction_tests.py
@@ -3,10 +3,6 @@ import unittest
 from fracturing.fraction import Fraction, ZeroDenominatorError
 
 
-def _f(a, b):
-    return Fraction(a, b)
-
-
 class FractionTestCase(unittest.TestCase):
     def test_can_create_fraction(self):
         f = Fraction(1, 2)
@@ -14,7 +10,7 @@ class FractionTestCase(unittest.TestCase):
 
     def test_can_print_fraction(self):
         # this is useful in test output
-        f = _f(1, 2)
+        f = Fraction(1, 2)
         assert str(f) == '1/2'
 
     def test_can_not_create_fraction_with_zero_denominator(self):
@@ -24,18 +20,18 @@ class FractionTestCase(unittest.TestCase):
 
 class FractionRepresentationTestCase(unittest.TestCase):
     def test_fractions_are_in_lowest_terms(self):
-        assert _f(2, 3) == _f(2, 3)
-        assert _f(4, 6) == _f(2, 3)
-        assert _f(5, 4) == _f(5, 4)
-        assert _f(15, 12) == _f(5, 4)
+        assert Fraction(2, 3) == Fraction(2, 3)
+        assert Fraction(4, 6) == Fraction(2, 3)
+        assert Fraction(5, 4) == Fraction(5, 4)
+        assert Fraction(15, 12) == Fraction(5, 4)
 
     def test_fractions_are_improper_and_not_mixed(self):
-        assert str(_f(37, 17)) != '2 3/17'
-        assert _f(37, 17) == _f(37, 17)
+        assert str(Fraction(37, 17)) != '2 3/17'
+        assert Fraction(37, 17) == Fraction(37, 17)
 
 
 class FractionEqualityTestCase(unittest.TestCase):
     def test_equality_of_fractions(self):
-        assert _f(2, 3) == _f(2, 3)
-        assert _f(2, 3) == _f(4, 6)
-        assert _f(2, 3) != _f(4, 7)
+        assert Fraction(2, 3) == Fraction(2, 3)
+        assert Fraction(2, 3) == Fraction(4, 6)
+        assert Fraction(2, 3) != Fraction(4, 7)
