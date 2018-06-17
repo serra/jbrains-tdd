8b183e1ebe09079f0f419bacd77cb4b392f3451c
Marijn van der Zee <marijn@serraict.com>
2018-06-17 11:04:30 +0200

    Simply increase code coverage

--- tests/fracturing_tests/coverage_tests.py
+++ tests/fracturing_tests/coverage_tests.py
@@ -6,3 +6,4 @@ from fracturing.fraction_covering import function_to_lower_code_coverage, anothe
 class SubtractFractionsTestCase(unittest.TestCase):
     def test_can_subtract_fractions(self):
         function_to_lower_code_coverage()
+        another_function_to_lower_coverage()
