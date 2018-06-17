6a1f86c5bce834e277145e80c3db2178eb815527
Marijn van der Zee <marijn@serraict.com>
2018-06-17 11:07:32 +0200

    Cleanup code coverage experiments
    Learned: explicitly include all modules in the test suite if you want to measure coverage.
    Modules that are not imported in the test suite (e.g. in context.py), are _not_ included in the coverage report.

--- fracturing/fraction_covering.py
+++
@@ -1,6 +0,0 @@
-def function_to_lower_code_coverage():
-    print('a line to lower coverage')
-
-
-def another_function_to_lower_coverage():
-    print('coverage dropped ... ')
--- tests/fracturing_tests/coverage_tests.py
+++
@@ -1,9 +0,0 @@
-from .context import fracturing
-import unittest
-from fracturing.fraction_covering import function_to_lower_code_coverage, another_function_to_lower_coverage
-
-
-class SubtractFractionsTestCase(unittest.TestCase):
-    def test_can_subtract_fractions(self):
-        function_to_lower_code_coverage()
-        another_function_to_lower_coverage()
