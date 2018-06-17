7c5735695c264d084244664d5d85e55873cab1ce
Marijn van der Zee <marijn@serraict.com>
2018-06-17 11:03:30 +0200

    Increase coverage

--- tests/fracturing_tests/coverage_tests.py
+++ tests/fracturing_tests/coverage_tests.py
@@ -1,3 +1,8 @@
 from .context import fracturing
 import unittest
-from fracturing.fraction_covering import *
+from fracturing.fraction_covering import function_to_lower_code_coverage, another_function_to_lower_coverage
+
+
+class SubtractFractionsTestCase(unittest.TestCase):
+    def test_can_subtract_fractions(self):
+        function_to_lower_code_coverage()
