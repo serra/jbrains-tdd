713a54580ac821ec00c062279b982c301f1f748e
Marijn van der Zee <marijn@serraict.com>
2018-06-18 16:56:52 +0200

    Add test that does not proof anything yet

--- S02/tests/pos_tests.py
+++ S02/tests/pos_tests.py
@@ -10,3 +10,8 @@ class TerminalTestCase(unittest.TestCase):
     def test_can_input_barcode(self):
         t = Terminal()
         t.on_barcode('a barcode')
+
+    @unittest.skip("this does not proof anything yet")
+    def test_displays_no_message_if_item_does_not_exist(self):
+        t = Terminal()
+        t.on_barcode('does not exist')
